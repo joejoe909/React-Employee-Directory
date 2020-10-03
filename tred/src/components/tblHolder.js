f6cdcccad1517414dbf2579c9ca160bf25421620
Joseph Farrish <joseph_a_f@hotmail.com>
2020-10-02 22:25:58 -0700

    addr

--- tred/src/components/tblHolder.js
+++ tred/src/components/tblHolder.js
@@ -1,13 +1,29 @@
 import React, {Component} from "react";
+import axios from "axios";
 import "./tblHolder.css";
 import API from "../util/API"
 // import EmpRow from "./EmpRow"
 
 class Holder extends Component {
-  componentDidMount() {
-    let emp = API.getUsers();
-    console.log(emp); //PromiseResult.data
+  state = {
+    search: "",
+    results:[]
+  };
 
+  constructRows(){
+    
+  }
+
+  componentDidMount() {
+    axios.get("https://randomuser.me/api/?results=50")
+    .then(res=>{
+      console.log(res.data.results)
+      let rslt = res.data.results;
+      this.results = rslt;
+      console.log(this.results);
+    })
+    .catch(err=> console.log(err));
+    
   }
 
   render() {
--- tred/src/util/API.js
+++ tred/src/util/API.js
@@ -2,10 +2,13 @@ import axios from "axios";
 
 export default{
     getUsers: ()=>{
-        axios.get("https://randomuser.me/api/?results=50&inc=picture&inc=name&inc=email&inc=phone&inc=dob")
-            .then(function res(){
+        axios.get("https://randomuser.me/api/?results=50")
+            .then(res=>{
                 console.log(res);
-                return JSON.stringify(res);
+                console.log(res.data);
+                let empList = res.data;
+                console.log(empList);
+                return(empList);
             });
     }
 
